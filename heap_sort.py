13de40069f3e96400901e7687313d1f4d0c27a00
sherwyndd <sherwynd2007@gmail.com>
2026-02-24 23:48:15 +0700

    Refactor heapsort to utilize numpy for array handling
    Updated heapsort to use numpy array for sorting and improved readability.

--- heap_sort.py
+++ heap_sort.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 def heapify(arr, n, i):
     largest = i  
     l = 2 * i + 1 
@@ -28,7 +29,8 @@ for i in range(1, numtest + 1):
             n = int(all_data[0])
             arr = [float(x) for x in all_data[1:]]
         start_time = time.time()
-        heapsort(arr)
+        heapsort(np.array(arr))
         end_time = time.time()
 
         print(end_time-start_time)
+
